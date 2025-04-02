fbbd4d61cb867a98ababa4d4e5a7597c55f1abb1
Mihael <mihael.pavlakovic@gmail.com>
2025-04-02 19:35:02 +0200

    chore: remove starting code

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import "./App.css";
 import { TypographyH1 } from "./components/typography/TypographyH1";
 
 function App() {
   return (
-    <>
+    <div className="flex items-center justify-center h-dvh">
       <TypographyH1>Vin-Manager App</TypographyH1>
-    </>
+    </div>
   );
 }
 
